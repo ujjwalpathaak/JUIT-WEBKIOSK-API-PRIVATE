57aac9479957cd72dcc3a413c29e2f908431fb22
Ujjwal Pathak <pathak2002ujjwal@gmail.com>
2023-03-04 01:52:47 +0530

    .

---
+++ server.js
@@ -0,0 +1,14 @@
+const request = require("request-promise");
+const cheerio = require("cheerio");
+
+const URL = "https://aws.amazon.com/certification/faqs/#:~:text=How%20much%20does%20an%20AWS,Specialty%20exams%20are%20300%20USD.";
+
+(async () => {
+  const response = await request(URL);
+
+  let $ = cheerio.load(response);
+
+  let trya = $('h1[id="AWS_Certification_FAQ"]').text();
+
+  console.log(trya);
+})();
